7b8deada364ce00d1ebc5f4aa2bc4fa7327eb59a
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2023-02-26 14:44:39 +0100

    added new example about updating observablelist

---
+++ src/main/java/edu/ntnu/idatt2001/lectures/table/simple/ex4/AddDialog.java
@@ -0,0 +1,67 @@
+package edu.ntnu.idatt2001.lectures.table.simple.ex4;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class AddDialog {
+
+  public void show(Stage primaryStage, ContactRegister contactRegister, ObservableList<Contact> contacts) {
+
+    GridPane grid = new GridPane();
+
+    Label firstnameLabel = new Label("Firstname: ");
+    TextField firstname = new TextField ();
+    grid.add(firstnameLabel,0,0);
+    grid.add(firstname,1,0);
+
+    Label lastnameLabel = new Label("Lastname: ");
+    TextField lastname = new TextField();
+    grid.add(lastnameLabel,0,1);
+    grid.add(lastname,1,1);
+
+    Label emailLabel = new Label("Email: ");
+    TextField email = new TextField();
+    grid.add(emailLabel,0,2);
+    grid.add(email,1,2);
+
+    Button ok = new Button("OK");
+
+    EventHandler<ActionEvent> event = (ActionEvent e) -> {
+
+            contactRegister.addContact(new Contact(firstname.getText(), lastname.getText(), email.getText()));
+            
+            contacts.setAll(contactRegister.getAllContacts());
+            Node source = (Node) e.getSource();
+            Stage stage = (Stage)source.getScene().getWindow();
+            stage.close();     
+    };
+
+    ok.setOnAction(event);
+  
+    grid.add(ok, 1, 3);
+
+    Scene dialogScene = new Scene(grid, 230, 100);
+
+    // New window (Stage)
+    Stage newWindow = new Stage();
+    newWindow.setTitle("Add contact");
+    newWindow.setScene(dialogScene);
+
+    // Set position of second window, related to primary window.
+    newWindow.setX(primaryStage.getX() + 200);
+    newWindow.setY(primaryStage.getY() + 100);
+    
+    Button btn = new Button();
+    btn.setText("Say 'Hello World' in the new windows");
+
+    newWindow.show();
+  }            
+}
--- src/main/java/edu/ntnu/idatt2001/lectures/table/simple/ex4/ContactRegisterApp.java
+++ src/main/java/edu/ntnu/idatt2001/lectures/table/simple/ex4/ContactRegisterApp.java
@@ -1,10 +1,11 @@
-package edu.ntnu.idatt2001.lectures.table.simple.ex3;
+package edu.ntnu.idatt2001.lectures.table.simple.ex4;
 
 import javafx.application.Application;
-import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -25,17 +26,13 @@ public class ContactRegisterApp extends Application {
 
     createTable();
 
-    ObservableList<Contact> list = getContacts();
-    list.addListener((Observable observable) -> System.out.println("List is invalidated"));
-
-    tableView.setItems(list);
+    ObservableList<Contact> contacts = getContacts();
+    tableView.setItems(contacts);
 
     tableView.setOnMouseClicked(mouseEvent -> {
       Contact selectedPerson = tableView.getSelectionModel().getSelectedItem();
       if (selectedPerson != null) {
         System.out.println(selectedPerson.getLastName());
-
-        tableView.getItems().add(new Contact("Kristina", "Hansen", "kristina.hansen@mail.com"));
       }
     });
 
@@ -43,6 +40,14 @@ public class ContactRegisterApp extends Application {
     BorderPane root = new BorderPane();
 
     root.setCenter(tableView);
+
+    Button btn = new Button();
+    btn.setText("Add");
+    btn.setOnAction((ActionEvent event) -> {
+      new AddDialog().show(primaryStage, contactRegister, contacts);
+    });
+
+    root.setBottom(btn);  
 
     Scene scene = new Scene(root, 310, 250);
     primaryStage.setScene(scene);
