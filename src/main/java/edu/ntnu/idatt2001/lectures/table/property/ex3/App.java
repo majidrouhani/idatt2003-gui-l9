1ae601e17d7513d85c77f8b678642dea478c3275
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2023-03-08 16:30:49 +0100

    use SimpleStringProperty

--- src/main/java/edu/ntnu/idatt2001/lectures/table/property/ex3/App.java
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.idatt2001.lectures.table.property.ex3;
-
-public class App {
-  public static void main(String[] args) {
-    PropertyExampleApp.main(args);
-  }
-}
--- src/main/java/edu/ntnu/idatt2001/lectures/table/property/ex4/App.java
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.idatt2001.lectures.table.property.ex4;
-
-public class App {
-  public static void main(String[] args) {
-    PropertyExampleApp.main(args);
-  }
-}
