52ab5c66c8b6f0edcf97f1ebc3b98b4e48105f58
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2023-03-08 16:01:52 +0100

    moved property folder outside of table

---
+++ src/main/java/edu/ntnu/idatt2001/lectures/property/ex3/App.java
@@ -0,0 +1,7 @@
+package edu.ntnu.idatt2001.lectures.property.ex3;
+
+public class App {
+  public static void main(String[] args) {
+    PropertyExampleApp.main(args);
+  }
+}
---
+++ src/main/java/edu/ntnu/idatt2001/lectures/property/ex3/PropertyExampleApp.java
@@ -0,0 +1,37 @@
+package edu.ntnu.idatt2001.lectures.property.ex3;
+
+import javafx.application.Application;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class PropertyExampleApp extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  public void start(Stage primaryStage) {
+
+    Pane pane = new Pane();
+
+    ReadOnlyDoubleProperty widthProperty = pane.widthProperty();
+    ReadOnlyDoubleProperty hightProperty = pane.heightProperty();
+
+    widthProperty.addListener((ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) -> 
+      System.out.println("widthProperty changed from " + oldVal.doubleValue() + " to " + newVal.doubleValue())
+    );
+
+    hightProperty.addListener((ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) -> 
+      System.out.println("hightProperty changed from " + oldVal.doubleValue() + " to " + newVal.doubleValue())
+    );
+
+    Scene scene = new Scene(pane, 200, 200, true);
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("2D Example");
+
+    primaryStage.show();
+  }
+}
