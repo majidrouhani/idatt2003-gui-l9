3a352a8a4f40f793a635cab1e3556d642b7d67bf
rouhani <majid.rouhani@ntnu.no>
2023-03-15 05:56:56 +0100

    move observable examples out of table-folder

---
+++ src/main/java/edu/ntnu/idatt2001/lectures/observable/ex1/ObservableText.java
@@ -0,0 +1,52 @@
+/* Online help: https://openjfx.io/openjfx-docs/#maven
+ * Non-modular from IDE
+ */
+
+package edu.ntnu.idatt2001.lectures.observable.ex1;
+
+import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class ObservableText extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("My first stage with scene");
+
+    TextField text1 = new TextField("First name");
+    text1.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+
+    TextField text2 = new TextField("Last name");
+    text2.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+
+    TextField text3 = new TextField("domain");
+    text3.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+
+
+    TextArea textArea = new TextArea();
+    textArea.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+
+    textArea.textProperty().bind(Bindings.concat(text1.textProperty(),".",text2.textProperty(),"@",text3.textProperty()));
+
+    VBox root = new VBox();
+    root.getChildren().addAll(text1);
+    root.getChildren().addAll(text2);
+    root.getChildren().addAll(text3);
+    root.getChildren().addAll(textArea);
+
+    primaryStage.setScene(new Scene(root, 400, 250));
+    primaryStage.show();
+  }
+
+}
---
+++ src/main/java/edu/ntnu/idatt2001/lectures/observable/ex2/App.java
@@ -0,0 +1,7 @@
+package edu.ntnu.idatt2001.lectures.observable.ex2;
+
+public class App {
+  public static void main(String[] args) {
+    ObservableListApp.main(args);
+  }
+}
